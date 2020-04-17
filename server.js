68b1690ac46c87df7c566165f52bec6bfacbe5a0
hitesh7737 <hitesh.lalwani@magicbricks.com>
2020-04-17 20:18:06 +0530

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const server = express();
-const proxy = require('http-proxy-middleware');
+const request = require('request');
 
 server.set('view engine', 'ejs');
 
-server.get('/', (req, res) => res.render('index'));
+server.get('/', (req, res) =>
+Promise.all([
+    getContents('http://localhost:8080/'),
+    getContents('http://localhost:8081/'),
+    getContents('http://localhost:8081/')
+]).then(responses =>
+res.render('index', { header: responses[0], productsList: responses[1], cart: responses[2] })
+).catch(error =>
+res.send(error.message)
+)
+);
 
-server.use('*', proxy({
-  target: 'https://microfrontends.herokuapp.com',
-  changeOrigin: true
-}));
+const getContents = (url) => new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+    if (error) return resolve("Error loading " + url + ": " + error.message);
 
-const port = process.env.PORT || 8080;
+return resolve(body);
+});
+});
+
+const port = process.env.PORT || 8082;
 server.listen(port, () => {
-  console.log(`Homepage listening on port ${port}`);
+    console.log(`Homepage listening on port ${port}`);
 });
